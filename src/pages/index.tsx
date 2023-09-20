ede19d5ed633141711620c7406f18fed72074eb5
EnzoCam7 <camerac09@gmail.com>
2023-09-20 15:49:00 -0300

    feature: added header and profile into homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import Container from "@/Components/Container"
 import Header from "@/Components/Header"
+import Profile from "@/Components/Profile"
 
 export default function Home() {
   return (
     <main>
       <Container>
         <Header/>
+        <Profile />
       </Container>
     </main>
   )
