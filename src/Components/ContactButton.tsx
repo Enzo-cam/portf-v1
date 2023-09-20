1f6c4b1cf5fa513b90e6383df8d87fe6c7ec282f
EnzoCam7 <camerac09@gmail.com>
2023-09-20 15:48:00 -0300

    feature: added contact button for email

---
+++ src/Components/ContactButton.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import { AiFillMail } from "react-icons/ai";
+
+const ContactButton = () => {
+  return (
+    <Link
+      href="mailto:enzo.fcam@gmail.com"
+      className="flex gap-4 items-center w-64 justify-center py-2 bg-marron text-white rounded hover:bg-negro transition-colors duration-300"
+    >
+      <AiFillMail />
+      Disponible para hablar
+    </Link>
+  );
+};
+
+export default ContactButton;
