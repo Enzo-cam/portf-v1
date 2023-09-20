92a68468963aaa8406bce764a713cd94a8b591e6
EnzoCam7 <camerac09@gmail.com>
2023-09-20 15:48:14 -0300

    add: Header component

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,22 +1,26 @@
-import Image from "next/image"
+import Image from "next/image";
+import Link from "next/link";
+import { BsGithub, BsLinkedin } from "react-icons/bs";
 
 const Header = () => {
   return (
-    <div>
-        <Image
-            src="/ryu.png"
-            alt="LogoEnzocam"
-            width={40}
-            height={40}
-        />
+    <div className="flex items-center mt-4 justify-between">
+      <div className="flex gap-4 items-center">
+        <Image src="/ryu.png" alt="LogoEnzocam" width={30} height={30} />
 
-        <p>
-            |
-        </p>
+        <p className="text-xl mt-1 text-gray-300">|</p>
+      </div>
 
-        
+      <div className="flex text-2xl gap-4">
+        <Link href="https://github.com/Enzo-cam" target="_blank">
+          <BsGithub />
+        </Link>
+        <Link href="https://www.linkedin.com/in/enzo-camera/" target="_blank">
+          <BsLinkedin />
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
