568f8af86404bf7915013e4c81c74cfcc0650e88
EnzoCam7 <camerac09@gmail.com>
2023-09-21 22:37:32 -0300

    fix: background color of button

--- src/Components/ContactButton.tsx
+++ src/Components/ContactButton.tsx
@@ -5,7 +5,7 @@ const ContactButton = () => {
   return (
     <Link
       href="mailto:enzo.fcam@gmail.com"
-      className="flex gap-4 items-center w-64 justify-center py-2 bg-marron text-white rounded hover:bg-negro transition-colors duration-300"
+      className="flex gap-4 items-center w-64 justify-center py-2 bg-negro text-white rounded hover:bg-marron transition-colors duration-300"
     >
       <AiFillMail />
       Disponible para hablar
